5cd87f861c9e46a0c3b28ca6e3c180843d196e44
jiyoungseo <aa4217121@gmail.com>
2025-01-17 14:07:59 +0000

    update

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1737122834',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
