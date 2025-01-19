dd3d2753206928b4c2a4c4b6942b8198d84bef14
jiyoungseo <aa4217121@gmail.com>
2025-01-19 07:11:33 +0000

    post image update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737269408',resources: [
+    cacheName: 'chirpy-1737270659',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
