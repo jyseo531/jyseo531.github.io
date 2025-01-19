f6b4eef43c68b682da6a6c58fb01fb9cd5bee388
jiyoungseo <aa4217121@gmail.com>
2025-01-19 06:50:26 +0000

    paper review post update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737268264',resources: [
+    cacheName: 'chirpy-1737269408',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
