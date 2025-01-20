c20ca1ab39209ae49efe24ae41a9f3d35e28f7ec
chimdungs <aa4217121@gmail.com>
2025-01-20 04:45:14 +0000

    image add

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737340782',resources: [
+    cacheName: 'chirpy-1737348294',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
