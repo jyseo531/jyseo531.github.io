fc0e4fbe847036354dac774758340cb7e80a88ae
chimdungs <aa4217121@gmail.com>
2025-01-20 02:38:41 +0000

    SuGaR paper review update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737270659',resources: [
+    cacheName: 'chirpy-1737340695',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
