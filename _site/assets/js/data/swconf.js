cf61cc34c186fac7a6519677b508f22958285faa
chimdungs <aa4217121@gmail.com>
2025-01-21 04:40:28 +0000

    marchingcube paper review update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737348294',resources: [
+    cacheName: 'chirpy-1737434416',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
