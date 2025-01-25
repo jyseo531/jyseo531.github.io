1db1f1ec5372fb44078d9f46bab1478a097d8ef1
jyseo531 <aa4217121@gmail.com>
2025-01-25 09:52:11 +0000

    graphics content update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737797388',resources: [
+    cacheName: 'chirpy-1737798726',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
