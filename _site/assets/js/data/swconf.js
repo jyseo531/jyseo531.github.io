c11367ef88f515290c13feee56d020aa5b72c8e5
jyseo531 <aa4217121@gmail.com>
2025-01-25 07:39:33 +0000

    likedin url update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737435744',resources: [
+    cacheName: 'chirpy-1737789984',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
