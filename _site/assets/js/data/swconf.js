25acf6faf5b619cdcd3885ec124957ef62343c56
jyseo531 <aa4217121@gmail.com>
2025-01-29 12:07:17 +0000

    chapter 3 post update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738151532',resources: [
+    cacheName: 'chirpy-1738152419',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
