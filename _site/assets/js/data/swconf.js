a6d3640f6f344c3ea26c7ea8d4cec99c72de0573
jyseo531 <aa4217121@gmail.com>
2025-02-07 03:28:46 +0000

    bug

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738898402',resources: [
+    cacheName: 'chirpy-1738898642',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
