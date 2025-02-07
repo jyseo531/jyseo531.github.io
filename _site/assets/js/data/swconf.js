0fe631012fdce38bf756774006f1527e4a590a3e
jyseo531 <aa4217121@gmail.com>
2025-02-07 03:22:10 +0000

    mode change

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738898052',resources: [
+    cacheName: 'chirpy-1738898402',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
