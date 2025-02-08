bc07bcc3bd33361087cc4cc6b62afb4f5d01a346
jyseo531 <aa4217121@gmail.com>
2025-02-08 02:40:30 +0000

    RaDe-GS

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738941746',resources: [
+    cacheName: 'chirpy-1738982422',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
