b9de71243ed24cf59ba8394ed3a202b0b77609c0
jyseo531 <aa4217121@gmail.com>
2025-02-08 11:39:16 +0000

    GOF update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739002986',resources: [
+    cacheName: 'chirpy-1739014749',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
