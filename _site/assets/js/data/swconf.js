080f50240bb1f66cce63627563a47b3a617831cb
chimdungs <aa4217121@gmail.com>
2025-02-10 07:56:54 +0000

    MipNeRF360 update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739084278',resources: [
+    cacheName: 'chirpy-1739174189',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
