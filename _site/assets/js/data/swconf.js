c2378468618f6441a27cbff3f59dbd62805f96f4
chimdungs <aa4217121@gmail.com>
2025-02-14 05:33:33 +0000

    chapter 7 update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739510731',resources: [
+    cacheName: 'chirpy-1739511209',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
