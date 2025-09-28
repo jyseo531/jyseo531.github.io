2615a205f60339e102c06414edb884b31f90fb10
jyseo531 <aa4217121@gmail.com>
2025-09-28 03:52:36 +0000

    pixie update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759020758',resources: [
+    cacheName: 'chirpy-1759031517',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
